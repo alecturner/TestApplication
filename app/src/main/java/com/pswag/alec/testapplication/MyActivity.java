d0e21786e0b8690fed5becaed55e68dfa0a7fcfc
Alec Turner <alec.turner@mail.mcgill.ca>
2015-07-22 21:40:08 -0700

    simple read/write to file. buttons and text fields used

--- app/src/main/java/com/pswag/alec/testapplication/MyActivity.java
+++ app/src/main/java/com/pswag/alec/testapplication/MyActivity.java
@@ -4,9 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
+import android.util.Log;
+import java.io.*;
+import android.view.inputmethod.InputMethodManager;
 
 public class MyActivity extends Activity {
+
+    final String datafile = "testdata.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +42,80 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //appends text to file
+    public void saveText(View view){
+
+        EditText et = (EditText) findViewById(R.id.editText);
+        String entry = (String) et.getText().toString();
+
+        //remove keyboard after entry
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+
+        writeToFile(entry,datafile);
+
+        //confirmation toast
+        Context context = getApplicationContext();
+        CharSequence text = "text added";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void readText(View view){
+
+        TextView tv = (TextView) findViewById(R.id.textView);
+        String data = readFromFile(datafile);
+        tv.setText(data);
+
+    }
+
+//copied from reply at http://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
+    private void writeToFile(String data, String filename) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(filename, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+//same stackoverflow response
+    private String readFromFile(String filename) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = openFileInput(filename);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 }
